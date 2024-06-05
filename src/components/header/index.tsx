412ed8ff3dc452524853853565a8ea25670793a1
Hamiani <hamiani.akbi@gmail.com>
2024-06-05 11:08:30 +0200

    responsive header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,45 +1,73 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 /* eslint-disable jsx-a11y/alt-text */
+import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Modal } from "antd";
 import "./header.css";
 
-import { useContext, useState } from "react";
-import { ThemeContext } from "../../context";
-import { Link } from "react-router-dom";
-
-const Headers = () => {
+const Header = () => {
+  const menuRef = useRef<HTMLDivElement | null>(null);
   const [isOpen, setIsOpen] = useState(false);
-  const context = useContext(ThemeContext);
-  const {
-    state: { blurValue },
-  } = context;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const onCloseMenu = () => setMenuOpen(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef, setMenuOpen]);
+
   return (
     <>
-      <header className="header" style={{ filter: `blur(${blurValue}px)` }}>
-        <img
-          src={"https://wemanity.com/img/menu/logo-wemanity-home-page.png"}
-        />
-        <nav className="topnav">
-          <Link to="/">Acceuil</Link>
-          <Link to="/history">Notre histoire</Link>
-          <Link to="/services">Nos services</Link>
-          <Link to="/team">Notre équipe</Link>
-          <Link to="/career">Site carrière </Link>
+      <header className="header">
+        <img src="https://wemanity.com/img/menu/logo-wemanity-home-page.png" />
+        <nav ref={menuRef} className={`topnav ${menuOpen ? "responsive" : ""}`}>
+          <div>
+            <Link to="/" onClick={onCloseMenu}>
+              Accueil
+            </Link>
+            <Link to="/history" onClick={onCloseMenu}>
+              Notre histoire
+            </Link>
+            <Link to="/services" onClick={onCloseMenu}>
+              Nos services
+            </Link>
+            <Link to="/team" onClick={onCloseMenu}>
+              Notre équipe
+            </Link>
+            <Link to="/career" onClick={onCloseMenu}>
+              Site carrière
+            </Link>
+          </div>
+          <div>
+            <button onClick={() => setIsOpen(true)}>Contact</button>
+          </div>
         </nav>
-        <button onClick={() => setIsOpen(true)}>contact</button>
+        <button className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          &#9776;
+        </button>
       </header>
       <Modal
         centered
         maskClosable={false}
         open={isOpen}
-        width={"100%"}
+        width="100%"
         footer={null}
         onCancel={() => setIsOpen(false)}
       >
         <iframe
-          width={"100%"}
-          height={"850"}
+          width="100%"
+          height="850"
           src="https://careers.wemanity.com/fr/contact/"
-          title="Présentation de l'équipe lilloise par practice"
           loading="lazy"
         />
       </Modal>
@@ -47,4 +75,4 @@ const Headers = () => {
   );
 };
 
-export default Headers;
+export default Header;
